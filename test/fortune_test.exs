e988cc10fdfc87974367922c333031db06bbf748
Masatoshi N <nishiguchi.masa@gmail.com>
2023-09-06 07:40:38 -0400

    support include/exclude options

--- test/fortune_test.exs
+++ test/fortune_test.exs
@@ -2,8 +2,28 @@ defmodule FortuneTest do
   use ExUnit.Case
   doctest Fortune
 
-  test "finding built-in fortunes" do
+  setup do
     priv_dir = Application.app_dir(:fortune, "priv")
-    assert Fortune.fortune_paths(priv_dir) == [Path.join(priv_dir, "elixir")]
+    %{priv_dir: priv_dir}
+  end
+
+  test "finding built-in fortunes", %{priv_dir: priv_dir} do
+    options = []
+    assert Fortune.fortune_paths(options) == [Path.join([priv_dir, "fortunes", "elixir"])]
+  end
+
+  test "finding fortunes with explicit fortunes paths", %{priv_dir: priv_dir} do
+    options = [paths: [Path.join([priv_dir, "fortunes"])]]
+    assert Fortune.fortune_paths(options) == [Path.join([priv_dir, "fortunes", "elixir"])]
+  end
+
+  test "finding fortunes with app inclusion list", %{priv_dir: priv_dir} do
+    options = [include: [:fortune]]
+    assert Fortune.fortune_paths(options) == [Path.join([priv_dir, "fortunes", "elixir"])]
+  end
+
+  test "finding fortunes with app exclusion list", %{priv_dir: _priv_dir} do
+    options = [exclude: [:fortune]]
+    assert Fortune.fortune_paths(options) == []
   end
 end
